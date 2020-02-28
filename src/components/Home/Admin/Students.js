b77ced33c05293337bda09ea16735ca26dd74dc3
Safdar Jamal <safdar.dev@gmail.com>
2020-02-29 00:13:59 +0500

    Create Students components for admin

--- src/components/Home/Admin/Students.js
+++ src/components/Home/Admin/Students.js
@@ -5,15 +5,34 @@ const Students = ({ students }) => {
   return (
     <div>
       <h1>Students</h1>
-      {students.map((student, i) => (
-        <p key={i}>{student.firstName}</p>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.email}</td>
+              <td>{student.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 Students.propTypes = {
-  students: PropTypes.object.isRequired
+  students: PropTypes.array.isRequired
 };
 
 export default Students;
--- src/containers/HomeContainer/AdminContainer/StudentsContainer.js
+++ src/containers/HomeContainer/AdminContainer/StudentsContainer.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../../../services/firebase';
 
 import Students from '../../../components/Home/Admin/Students';
 
 class StudentsContainer extends Component {
+  state = { students: [] };
+
+  componentDidMount() {
+    const { firebase } = this.props;
+
+    firebase
+      .getStudents()
+      .then(querySnapshot => {
+        const students = [];
+
+        querySnapshot.forEach(doc => students.push(doc.data()));
+
+        this.setState({ students });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
-    return <Students />;
+    return <Students students={this.state.students} />;
   }
 }
 
-export default StudentsContainer;
+export default withFirebase(StudentsContainer);
