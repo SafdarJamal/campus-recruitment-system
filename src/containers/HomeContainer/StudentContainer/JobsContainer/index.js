38f624fc4b42304cde6f73d7e754b134772a5945
Safdar Jamal <safdar.dev@gmail.com>
2020-02-28 23:38:49 +0500

    Create Jobs components for student

---
+++ src/containers/HomeContainer/StudentContainer/JobsContainer/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { withFirebase } from '../../../../services/firebase';
+
+import Jobs from '../../../../components/Home/Student/Jobs';
+
+class JobsContainer extends Component {
+  state = { jobs: [] };
+
+  componentDidMount() {
+    const { firebase } = this.props;
+
+    firebase
+      .getJobs()
+      .then(querySnapshot => {
+        let jobs = [];
+
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+
+          if (data.jobs) {
+            jobs = jobs.concat(data.jobs);
+          }
+        });
+
+        this.setState({ jobs });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return <Jobs jobs={this.state.jobs} />;
+  }
+}
+
+export default withFirebase(JobsContainer);
