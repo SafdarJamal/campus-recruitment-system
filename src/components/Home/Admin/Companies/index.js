5ff4957a532b2488ed9faade4942fb6b87165054
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 21:12:30 +0500

    Create CompaniesTable UI for admin

--- src/components/Home/Admin/Companies/index.js
+++ src/components/Home/Admin/Companies/index.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 const Companies = ({ companies, handleDelete }) => {
   return (
-    <div>
-      <h1>Companies</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {companies.map((company, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{company.companyName}</td>
-              <td>{company.companyEmail}</td>
-              <td>{company.companyPhone}</td>
-              <td>
-                <button data-id={company.id} onClick={handleDelete}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container>
+      <Card>
+        <Card.Header as="h2" className="text-center">
+          Companies
+        </Card.Header>
+        <Card.Body>
+          <Table bordered hover>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {companies.map((company, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{company.companyName}</td>
+                  <td>{company.companyEmail}</td>
+                  <td>{company.companyPhone}</td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      data-id={company.id}
+                      onClick={handleDelete}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
