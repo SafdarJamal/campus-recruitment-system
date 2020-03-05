cb6a87cb561efd03ef288b3fe9c9eec23f70de24
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 22:05:55 +0500

    Create CompaniesTable UI for student

--- src/components/Home/Student/Companies/index.js
+++ src/components/Home/Student/Companies/index.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 const Companies = ({ companies }) => {
   return (
-    <div>
-      <h1>Companies</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {companies.map((company, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{company.companyName}</td>
-              <td>{company.companyEmail}</td>
-              <td>{company.companyPhone}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container>
+      <Card className="shadow-sm">
+        <Card.Header as="h2" className="text-center">
+          Companies
+        </Card.Header>
+        <Card.Body>
+          <Table bordered hover>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {companies.map((company, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{company.companyName}</td>
+                  <td>{company.companyEmail}</td>
+                  <td>{company.companyPhone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
