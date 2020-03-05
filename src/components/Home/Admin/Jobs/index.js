6b0cd4f9a4afae309df3653fbc807ed88e442e4f
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 21:20:35 +0500

    Create JobsTable UI for admin

--- src/components/Home/Admin/Jobs/index.js
+++ src/components/Home/Admin/Jobs/index.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 const Jobs = ({ jobs }) => {
   return (
-    <div>
-      <h1>Jobs</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Title</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jobs.map((job, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{job.title}</td>
-              <td>{job.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container>
+      <Card>
+        <Card.Header as="h2" className="text-center">
+          Students
+        </Card.Header>
+        <Card.Body>
+          <Table bordered hover>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {jobs.map((job, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{job.title}</td>
+                  <td>{job.description}</td>
+                  <td>
+                    <Button variant="danger" data-id={job.id}>
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
