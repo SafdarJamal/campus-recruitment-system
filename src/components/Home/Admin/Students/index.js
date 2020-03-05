8eb365539c48ce0aa3cf3a079164274ba315d20f
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 21:15:45 +0500

    Create StudentsTable UI for admin

--- src/components/Home/Admin/Students/index.js
+++ src/components/Home/Admin/Students/index.js
@@ -1,39 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 const Students = ({ students, handleDelete }) => {
   return (
-    <div>
-      <h1>Students</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{student.firstName}</td>
-              <td>{student.lastName}</td>
-              <td>{student.email}</td>
-              <td>{student.phone}</td>
-              <td>
-                <button data-id={student.id} onClick={handleDelete}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container>
+      <Card>
+        <Card.Header as="h2" className="text-center">
+          Students
+        </Card.Header>
+        <Card.Body>
+          <Table bordered hover>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((student, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{student.firstName}</td>
+                  <td>{student.lastName}</td>
+                  <td>{student.email}</td>
+                  <td>{student.phone}</td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      data-id={student.id}
+                      onClick={handleDelete}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
