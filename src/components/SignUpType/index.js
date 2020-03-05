903056fd91b4e5dc3a52bb949663724bbde66157
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 20:33:14 +0500

    Create SignUpType UI

--- src/components/SignUpType/index.js
+++ src/components/SignUpType/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import * as ROUTES from '../../constants/routes';
 
 const SignUpType = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to={ROUTES.SIGN_UP_COMPANY}>Sign Up as Company</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.SIGN_UP_STUDENT}>Sign Up as Student</Link>
-        </li>
-      </ul>
-    </div>
+    <Container className="col-md-4">
+      <Card className="shadow-sm">
+        <Card.Header as="h2" className="text-center">
+          Sign Up As
+        </Card.Header>
+
+        <Card.Body>
+          <LinkContainer to={ROUTES.SIGN_UP_COMPANY}>
+            <Button variant="light" size="lg" block>
+              Company
+            </Button>
+          </LinkContainer>
+          <LinkContainer to={ROUTES.SIGN_UP_STUDENT}>
+            <Button variant="light" size="lg" block>
+              Student
+            </Button>
+          </LinkContainer>
+        </Card.Body>
+
+        <Card.Footer className="text-muted text-center">
+          Already have an account? <Link to={ROUTES.LOG_IN}>Log In</Link>
+        </Card.Footer>
+      </Card>
+    </Container>
   );
 };
 
