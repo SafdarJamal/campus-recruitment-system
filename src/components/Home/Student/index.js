a131d91b849e331c7fb82bbe3ab94a21bb064fb2
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 20:46:48 +0500

    Create Student home UI

--- src/components/Home/Student/index.js
+++ src/components/Home/Student/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const Student = () => {
   return (
-    <div>
-      <h1>Student Home</h1>
-    </div>
+    <Container>
+      <Jumbotron className="shadow-sm">
+        <h1>Student Home</h1>
+      </Jumbotron>
+    </Container>
   );
 };
 
