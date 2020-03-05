275bdb2aeaf65def3b2d5a167a6ade0ac41e19e8
Safdar Jamal <safdar.dev@gmail.com>
2020-03-05 19:51:51 +0500

    Create SignUp UI

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,43 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 
 const SignUp = ({ handleChange, handleSubmit, error }) => {
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <p>{error}</p>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        <br />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <Container className="col-md-4">
+      <Card className="shadow-sm">
+        <Card.Header as="h2" className="text-center">
+          Sign Up
+        </Card.Header>
+
+        <Card.Body>
+          <Alert variant="danger" show={error}>
+            {error}
+          </Alert>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="firstName"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="lastName"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                name="email"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                name="password"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+
+        <Card.Footer className="text-muted text-center">
+          Already have an account? <Link to={ROUTES.LOG_IN}>Log In</Link>
+        </Card.Footer>
+      </Card>
+    </Container>
   );
 };
 
