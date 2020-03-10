31a3c5afc4485e0a9791e62e45aede4aa527a2fe
Safdar Jamal <safdar.dev@gmail.com>
2020-03-10 20:56:15 +0500

    Create student model

---
+++ server/models/Student.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Students', StudentSchema);
