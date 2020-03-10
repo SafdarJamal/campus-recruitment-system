52a9882346c19591ddefefda1edb914aab019f79
Safdar Jamal <safdar.dev@gmail.com>
2020-03-10 20:56:03 +0500

    Create company model

---
+++ server/models/Company.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const CompanySchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  companyName: {
+    type: String,
+    required: true
+  },
+  companyEmail: {
+    type: String,
+    required: true
+  },
+  companyPhone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Companies', CompanySchema);
