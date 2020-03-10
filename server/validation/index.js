7036545f107c94644017c248d365b65f4e325311
Safdar Jamal <safdar.dev@gmail.com>
2020-03-10 23:50:20 +0500

    Create LogIn validation

--- server/validation/index.js
+++ server/validation/index.js
@@ -40,5 +40,21 @@ const validateStudentSignUp = data => {
   return schema.validate(data);
 };
 
+const validateLogIn = data => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net'] }
+      })
+      .required(),
+    password: Joi.string()
+      .pattern(new RegExp('^[a-zA-Z0-9]{6,64}$'))
+      .required()
+  });
+
+  return schema.validate(data);
+};
 module.exports.validateCompanySignUp = validateCompanySignUp;
 module.exports.validateStudentSignUp = validateStudentSignUp;
+module.exports.validateLogIn = validateLogIn;
