2b031762504cc7c15cafa0854ddc62605bd62075
Safdar Jamal <safdar.dev@gmail.com>
2020-03-11 20:48:45 +0500

    Create auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) return res.status(401).send('Access denied.');
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid token.');
+  }
+};
+
+module.exports = auth;
