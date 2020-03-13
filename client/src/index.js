90adfb8b5a3201b297015cc7b2862c5fc32e3f8b
Safdar Jamal <safdar.dev@gmail.com>
2020-03-13 22:05:47 +0500

    Integrate api service

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import Firebase, { FirebaseContext } from './services/firebase';
+import API, { APIContext } from './services/api';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './containers/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,9 +15,11 @@ ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
       <FirebaseContext.Provider value={new Firebase()}>
-        <Router>
-          <App />
-        </Router>
+        <APIContext.Provider value={new API()}>
+          <Router>
+            <App />
+          </Router>
+        </APIContext.Provider>
       </FirebaseContext.Provider>
     </PersistGate>
   </Provider>,
