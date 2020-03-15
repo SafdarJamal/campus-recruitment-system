70b45ba8afe9cd5db1aaa5e1bdfc9aa1d6a76315
Safdar Jamal <safdar.dev@gmail.com>
2020-03-15 21:21:48 +0500

    Create NotFound UI

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-    </div>
+    <Container className="col-md-4">
+      <Card className="shadow-sm text-center">
+        <Card.Body>
+          <h1>404 - Not Found</h1>
+          <br />
+          <Link to={ROUTES.LANDING} style={{ textDecoration: 'none' }}>
+            <Button variant="success" size="lg" block>
+              Go to Home
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
