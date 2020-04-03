2c5344822bb9e3826ec4010b19fb246d218cc714
Safdar Jamal <safdar.dev@gmail.com>
2020-04-03 20:16:45 +0500

    Implement job-application api for student

--- client/src/containers/HomeContainer/StudentContainer/JobsContainer/index.js
+++ client/src/containers/HomeContainer/StudentContainer/JobsContainer/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { withAPI } from '../../../../services/api';
 
 import Jobs from '../../../../components/Home/Student/Jobs';
@@ -21,9 +23,28 @@ class JobsContainer extends Component {
       .catch(error => console.log(error.response.data.message));
   };
 
+  handleApply = e => {
+    const { api } = this.props;
+
+    api
+      .applyToJob(e.target.dataset.id)
+      .then(() => this.getJobs())
+      .catch(error => console.log(error.response.data.message));
+  };
+
   render() {
-    return <Jobs jobs={this.state.jobs} />;
+    return (
+      <Jobs
+        _id={this.props._id}
+        jobs={this.state.jobs}
+        handleApply={this.handleApply}
+      />
+    );
   }
 }
 
-export default withAPI(JobsContainer);
+const mapStateToProps = state => {
+  return { _id: state.user._id };
+};
+
+export default compose(connect(mapStateToProps), withAPI)(JobsContainer);
