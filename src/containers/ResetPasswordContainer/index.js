0c124f71d10a2a1b8415d6f7b8d00d6ac31a19f6
Safdar Jamal <safdar.dev@gmail.com>
2020-04-30 20:58:20 +0500

    Create ResetPassword component

---
+++ src/containers/ResetPasswordContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import ResetPassword from '../../components/ResetPassword';
+
+class ResetPasswordContainer extends Component {
+  render() {
+    return <ResetPassword />;
+  }
+}
+
+export default ResetPasswordContainer;
