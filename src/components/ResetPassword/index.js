6b47fceb3f2ca1cb6f644fe7db81d9e3af8d7a82
Safdar Jamal <safdar.dev@gmail.com>
2020-04-30 21:39:51 +0500

    Create ResetPassword container

--- src/components/ResetPassword/index.js
+++ src/components/ResetPassword/index.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 
-const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+const ResetPassword = ({
+  email,
+  handleChange,
+  handleSubmit,
+  success,
+  error
+}) => {
+  return (
+    <Container className="col-md-4">
+      <Card className="shadow-sm">
+        <Card.Header as="h2" className="text-center">
+          Reset Password
+        </Card.Header>
+        <Card.Body>
+          <Alert variant="success" show={success}>
+            Password reset link has been send to your provided email address,
+            check you mailbox.
+          </Alert>
+          <Alert variant="danger" show={error}>
+            {error}
+          </Alert>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Email Me Reset Link
+            </Button>
+          </Form>
+        </Card.Body>
+        <Card.Footer className="text-muted text-center">
+          Back to <Link to={ROUTES.LOG_IN}>Log In</Link>
+        </Card.Footer>
+      </Card>
+    </Container>
+  );
+};
+
+ResetPassword.propTypes = {
+  email: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  success: PropTypes.bool,
+  error: PropTypes.string
 };
 
 export default ResetPassword;
--- src/containers/ResetPasswordContainer/index.js
+++ src/containers/ResetPasswordContainer/index.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../../services/firebase';
 
 import ResetPassword from '../../components/ResetPassword';
 
 class ResetPasswordContainer extends Component {
+  state = {
+    email: '',
+    success: false,
+    error: null
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { firebase } = this.props;
+    const { email } = this.state;
+
+    firebase
+      .resetPassword(email)
+      .then(() => this.setState({ success: true, error: null }))
+      .catch(error => this.setState({ success: false, error: error.message }));
+  };
+
   render() {
-    return <ResetPassword />;
+    const { email, success, error } = this.state;
+
+    return (
+      <ResetPassword
+        email={email}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        success={success}
+        error={error}
+      />
+    );
   }
 }
 
-export default ResetPasswordContainer;
+export default withFirebase(ResetPasswordContainer);
