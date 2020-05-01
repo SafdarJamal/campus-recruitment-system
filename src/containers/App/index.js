4d7f7f2562c96793c2d6330a7b662fb3a09b135a
Safdar Jamal <safdar.dev@gmail.com>
2020-05-02 01:05:25 +0500

    Integrate error boundary

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import ErrorBoundary from '../../components/ErrorBoundary';
 import HeaderContainer from '../HeaderContainer';
 import Routes from '../../routes';
 import Footer from '../../components/Footer';
 
 const App = () => {
   return (
-    <>
+    <ErrorBoundary>
       <HeaderContainer />
       <Routes />
       <Footer />
-    </>
+    </ErrorBoundary>
   );
 };
 
