d09d073b5b3caad6ed67a1d749e3b468dcdd7dd8
Safdar Jamal <safdar.dev@gmail.com>
2020-05-21 22:37:04 +0500

    Integrate Layout in App component

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,19 +1,18 @@
 import React, { Suspense } from 'react';
 
 import ErrorBoundary from '../../components/ErrorBoundary';
-import HeaderContainer from '../HeaderContainer';
+import Layout from '../../components/Layout';
 import Spinner from '../../components/Spinner';
 import Routes from '../../routes';
-import Footer from '../../components/Footer';
 
 const App = () => {
   return (
     <ErrorBoundary>
-      <HeaderContainer />
-      <Suspense fallback={<Spinner />}>
-        <Routes />
-      </Suspense>
-      <Footer />
+      <Layout>
+        <Suspense fallback={<Spinner />}>
+          <Routes />
+        </Suspense>
+      </Layout>
     </ErrorBoundary>
   );
 };
