e5aaf64ff0c7fffcc886a88c154945ecb794b34c
Safdar Jamal <safdar.dev@gmail.com>
2020-05-21 22:36:28 +0500

    Create Layout component

---
+++ client/src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import HeaderContainer from '../../containers/HeaderContainer';
+import Footer from '../Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <HeaderContainer />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+export default Layout;
