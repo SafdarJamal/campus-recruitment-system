4485637f090fe384805ed8f99b634e1da148cd7c
Safdar Jamal <safdar.dev@gmail.com>
2020-06-05 19:40:15 +0500

    Modularize validation code

---
+++ server/src/validation/validateLogIn.js
@@ -0,0 +1,12 @@
+const Joi = require('@hapi/joi');
+
+const validateLogIn = data => {
+  const schema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,64}$')).required()
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = validateLogIn;
---
+++ server/src/validation/validateSignUp.js
@@ -0,0 +1,14 @@
+const Joi = require('@hapi/joi');
+
+const validateSignUp = data => {
+  const schema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,64}$')).required()
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = validateSignUp;
