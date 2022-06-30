84601adfe6994f13ff5cddb5908fbd2642ceb896
Safdar Jamal <safdar.dev@gmail.com>
2022-07-01 00:16:23 +0500

    Format validation code

--- server/src/validation/validateLogIn.js
+++ server/src/validation/validateLogIn.js
@@ -3,7 +3,9 @@ const Joi = require('joi');
 const validateLogIn = data => {
   const schema = Joi.object({
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,64}$')).required()
+    password: Joi.string()
+      .pattern(new RegExp('^[a-zA-Z0-9]{6,64}$'))
+      .required(),
   });
 
   return schema.validate(data);
