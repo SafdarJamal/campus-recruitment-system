ce8fbabfc4c65669890daa5b12be364025b9ca85
Safdar Jamal <safdar.dev@gmail.com>
2022-07-03 22:26:01 +0500

    Update Landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -10,7 +10,7 @@ const Landing = () => {
       <div className="p-5 mb-4 bg-light rounded-3">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold">Recruit faster with CRS!</h1>
-          <p className="col-md-8 fs-4">
+          <p className="col-md-12 fs-4">
             Campus Recruitment System (CRS) is a recruiting app that makes the
             hiring process faster and simpler so that your team can hire the
             best people, faster.
