98ab8bb840a1f29c7d161fcfc638cb2a60c90e36
Safdar Jamal <safdar.dev@gmail.com>
2022-11-16 00:23:51 +0500

    Update Admin Routes

--- client/src/containers/HomeContainer/AdminContainer/index.js
+++ client/src/containers/HomeContainer/AdminContainer/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import * as ROUTES from '../../../constants/routes';
+// import * as ROUTES from '../../../constants/routes';
 
 import Admin from '../../../components/Home/Admin/Lazy';
 import CompaniesContainer from './CompaniesContainer/Lazy';
@@ -15,11 +15,11 @@ class AdminContainer extends Component {
     return (
       <Routes>
         <Route index element={<Admin />} />
-        <Route path={ROUTES.COMPANIES} element={<CompaniesContainer />} />
-        <Route path={ROUTES.STUDENTS} element={<StudentsContainer />} />
-        <Route path={ROUTES.JOBS} element={<JobsContainer />} />
-        <Route path={ROUTES.PROFILE} element={<ProfileContainer />} />
-        <Route path={ROUTES.PROFILE_EDIT} element={<EditContainer />} />
+        <Route path="companies" element={<CompaniesContainer />} />
+        <Route path="students" element={<StudentsContainer />} />
+        <Route path="jobs" element={<JobsContainer />} />
+        <Route path="profile" element={<ProfileContainer />} />
+        <Route path="profile/edit" element={<EditContainer />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     );
