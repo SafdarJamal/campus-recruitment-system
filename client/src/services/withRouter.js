445334719e590c4e9d428ad4e808995549e65a7f
Safdar Jamal <safdar.dev@gmail.com>
2022-11-19 00:14:19 +0500

    Refactor withRouter

--- client/src/services/withRouter.js
+++ client/src/services/withRouter.js
@@ -1,11 +1,12 @@
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 const withRouter = Component => props => {
-  let location = useLocation();
-  let navigate = useNavigate();
-  let params = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const params = useParams();
+  const router = { location, navigate, params };
 
-  return <Component {...props} router={{ location, navigate, params }} />;
+  return <Component {...props} {...router} />;
 };
 
 export default withRouter;
